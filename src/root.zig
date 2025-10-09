6e75c687dad59008bbfa931279813d2058c60665
Ryan Eicher <ryan.eicher@bd.com>
2025-10-09 15:26:32 -0400

    This was the orig working sample

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 const azure = @cImport({
-    @cDefine("USE_EDGE_MODULES", "1");
     @cInclude("azure_macro_utils/macro_utils.h");
     @cInclude("umock_c/umock_c_prod.h");
     @cInclude("azure_c_shared_utility/platform.h");
@@ -9,8 +8,10 @@ const azure = @cImport({
     @cInclude("azure_c_shared_utility/tickcounter.h");
     @cInclude("azure_c_shared_utility/xlogging.h");
     @cInclude("iothub.h");
-    @cInclude("iothub_module_client.h");
+    @cInclude("iothub_client_core_ll.h");
+    @cInclude("iothub_module_client_ll.h");
     @cInclude("iothubtransportmqtt.h");
+    @cInclude("iothub_client_core_common.h");
     @cInclude("iothub_client_options.h");
 });
 
@@ -20,26 +21,20 @@ pub const IoTError = error{
     CreateClientFailed,
 };
 
-pub const ModuleClient = struct {
-    handle: azure.IOTHUB_MODULE_CLIENT_HANDLE,
+pub fn initializeModule(allocator: std.mem.Allocator, connection_string: []const u8) IoTError!void {
+    const connection_z = try allocator.dupeZ(u8, connection_string);
+    defer allocator.free(connection_z);
 
-    pub fn deinit(self: *ModuleClient) void {
-        azure.IoTHubModuleClient_Destroy(self.handle);
-        azure.IoTHub_Deinit();
-    }
-};
-
-pub fn createModuleClientFromEnvironment() IoTError!ModuleClient {
     if (azure.IoTHub_Init() != 0) {
         return IoTError.IoTHubInitFailed;
     }
+    defer azure.IoTHub_Deinit();
 
     const transport: azure.IOTHUB_CLIENT_TRANSPORT_PROVIDER = azure.MQTT_Protocol;
-    const handle = azure.IoTHubModuleClient_CreateFromEnvironment(transport);
-    if (handle == null) {
-        azure.IoTHub_Deinit();
+    const module_handle = azure.IoTHubModuleClient_LL_CreateFromConnectionString(connection_z.ptr, transport);
+    if (module_handle == null) {
         return IoTError.CreateClientFailed;
     }
-
-    return ModuleClient{ .handle = handle.? };
+    defer azure.IoTHubModuleClient_LL_Destroy(module_handle);
+    std.debug.print("WE DID THINGS WITHOUT GETTING AN ERROR WHAAAAAAAAAAAAAAAT", .{});
 }
