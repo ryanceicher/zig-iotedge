cf14a0b4bdadd0393407176ee3b47ed7d03de423
ryanceicher <ryanceicher@gmail.com>
2025-10-15 16:51:44 -0400

    idk

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,11 @@ const std = @import("std");
 const zig_iotedge = @import("zig_iotedge");
 const nats = @import("nats");
 
+// TODO we need to remove option1,2,3 and replace it with parameters for connecting to our nats server
 const TwinEnvelope = struct {
     desired: struct {
         @"$version": i64 = 0,
+        // TODO REPLACE THESE OPTIONS
         option1: bool = false,
         option2: []const u8 = "",
         option3: i32 = 0,
@@ -13,6 +15,59 @@ const TwinEnvelope = struct {
         @"$version": i64 = 0,
     } = .{},
 };
+
+pub fn main() !void {
+    // TODO we need to check for IOTEDGE environment variables, if they arent present then
+    // we should run without any of the iotedge related stuff and expect a json config file with how we need to connect to our nats server
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const env = try std.process.getEnvMap(arena.allocator());
+
+    var iterator = env.iterator();
+
+    while (iterator.next()) |next| {
+        std.debug.print("Env Variables", .{});
+        std.debug.print("   {s}:{s}", .{ &next.key_ptr, &next.value_ptr });
+    }
+
+    std.debug.print("Creating Azure IoT Edge module client from environment...\n", .{});
+
+    const client_attempt = try zig_iotedge.createModuleClientFromEnvironment();
+
+    if (client_attempt) |client_value| {
+        var client = client_value;
+        defer client.deinit();
+
+        if (client.enableLogTrace(true)) |_| {} else |err| {
+            std.debug.print("Failed to enable log tracing ({s}). Continuing without it.\n", .{@errorName(err)});
+        }
+
+        if (client.setAutoUrlEncodeDecode(true)) |_| {} else |err| {
+            std.debug.print("Failed to enable auto URL encoding ({s}).\n", .{@errorName(err)});
+        }
+
+        if (client.setDoWorkFrequencyMs(10)) |_| {} else |err| {
+            std.debug.print("Failed to set DoWork frequency ({s}).\n", .{@errorName(err)});
+        }
+
+        if (client.onConnectionStatus(&connectionStatusHandler)) |_| {} else |err| {
+            std.debug.print("Failed to register connection status callback ({s}).\n", .{@errorName(err)});
+        }
+
+        if (client.onTwinEx(&twinHandlerEx)) |_| {} else |err| {
+            std.debug.print("Failed to set twin callback ({s}).\n", .{@errorName(err)});
+        }
+
+        if (client.requestTwinSnapshot()) |_| {} else |err| {
+            std.debug.print("Failed to get twin snapshot ({s}).\n", .{@errorName(err)});
+        }
+
+        std.debug.print("Module client configured. Waiting for work...\n", .{});
+        keepAliveLoop();
+    }
+}
 
 fn twinHandlerEx(client: *zig_iotedge.ModuleClient, kind: zig_iotedge.TwinUpdateKind, bytes: []const u8) void {
     const prefix = switch (kind) {
@@ -52,7 +107,7 @@ fn connectionStatusHandler(status: zig_iotedge.ConnectionStatus, reason_code: u3
         .unauthenticated => "unauthenticated",
         .unknown => "unknown",
     };
-    
+
     std.debug.print("Connection status update: {s} (reason={d})\n", .{ desc, reason_code });
 }
 
@@ -81,49 +136,4 @@ fn callback(_: *bool, _: *nats.Connection, _: *nats.Subscription, msg: *nats.Mes
     const subject = msg.getSubject();
     const data = msg.getData() orelse "";
     std.debug.print("Received a message on subject {s}: {s}\n", .{ subject, data });
-}
-
-pub fn main() !void {
-    std.debug.print("Creating Azure IoT Edge module client from environment...\n", .{});
-
-    const client_attempt = zig_iotedge.createModuleClientFromEnvironment();
-
-    if (client_attempt) |client_value| {
-        var client = client_value;
-        defer client.deinit();
-
-        if (client.enableLogTrace(true)) |_| {} else |err| {
-            std.debug.print("Failed to enable log tracing ({s}). Continuing without it.\n", .{@errorName(err)});
-        }
-
-        if (client.setAutoUrlEncodeDecode(true)) |_| {} else |err| {
-            std.debug.print("Failed to enable auto URL encoding ({s}).\n", .{@errorName(err)});
-        }
-
-        if (client.setDoWorkFrequencyMs(10)) |_| {} else |err| {
-            std.debug.print("Failed to set DoWork frequency ({s}).\n", .{@errorName(err)});
-        }
-
-        if (client.onConnectionStatus(&connectionStatusHandler)) |_| {} else |err| {
-            std.debug.print("Failed to register connection status callback ({s}).\n", .{@errorName(err)});
-        }
-
-        // Register for desired property updates and request the full twin snapshot
-        if (client.onTwinEx(&twinHandlerEx)) |_| {} else |err| {
-            std.debug.print("Failed to set twin callback ({s}).\n", .{@errorName(err)});
-        }
-
-        if (client.requestTwinSnapshot()) |_| {} else |err| {
-            std.debug.print("Failed to get twin snapshot ({s}).\n", .{@errorName(err)});
-        }
-
-        std.debug.print("Module client configured. Waiting for work...\n", .{});
-        keepAliveLoop();
-    } else |err| {
-        std.debug.print(
-            "Module client creation failed ({s}). Staying alive for baseline observation.\n",
-            .{@errorName(err)},
-        );
-        keepAliveLoop();
-    }
 }
